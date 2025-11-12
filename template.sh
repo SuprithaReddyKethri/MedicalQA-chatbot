7f6418692085be3ca3259afeaf05a70383f6bbd3
SuprithaReddyKethri <kethrisuprithareddy23csds@rnsit.ac.in>
2025-11-12 15:16:12 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,10 @@
+mkdir -p src
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+echo "Directory and files created successfully"
