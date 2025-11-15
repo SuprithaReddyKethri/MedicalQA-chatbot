093af67a150d51955e4408083708c81c5e51045c
SuprithaReddyKethri <kethrisuprithareddy23csds@rnsit.ac.in>
2025-11-15 13:57:51 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-#mymedicalchatbot
+git clone: https://github.com/entbappy/Build-a-Complete-Medical-Chatbot-with-LLMs-LangChain-Pinecone-Flask-AWS.git
+
+### creating environmentt
+conda create -n chattbot python=3.11 -y
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical chatbot",
+    version="0.1.0",
+    author="Harini, Supritha, Harshitha",
+    author_email="kethrisuprithareddy23csds@rnsit.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
